1dddeddb63d801e9be5e20078905bfe0e061cc16
Shrivu Shankar <shrivu1122@gmail.com>
2018-07-07 15:46:20 -0500

    Added More Projects
    Tweaked Lib

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,50 @@
 
 $(() => {
 
+  let years = {}
+
+  projects.sort((a, b) => {
+    return new Date(b.start).getTime() - new Date(a.start).getTime()
+  })
+
   for(let project of projects) {
 
-    $('#timeline-container').append(`<section class="year">
-      <h3>${new Date(project.start).getFullYear()}</h3>
-      <section>
-        <ul>
-        <li>
-          <b>${project.title}</b> <hr>
-          ${project.tagline}
-        </li>
-        </ul>
-      </section>
-    </section>`)
+    let curYear = new Date(project.start).getFullYear()
+
+    if(!years[curYear]) {
+
+      years[curYear] = []
+
+    }
+
+    years[curYear].push(project);
 
   }
+
+  let html = '';
+
+  Object.keys(years).sort().reverse().forEach((year) => {
+
+    html += `<section class="year"><h3>${year}</h3>`
+
+    for(let project of years[year]) {
+
+      html += `
+        <section>
+          <ul>
+          <li>
+            <b>${project.title}</b> <hr>
+            ${project.tagline}
+          </li>
+          </ul>
+        </section>`
+
+    }
+
+    html += '</section>'
+
+  });
+
+  $('#timeline-container').append(html);
 
 })
--- app/js/projects.js
+++ app/js/projects.js
@@ -3,11 +3,119 @@ const projects = [
   {
     'title': 'Cy-Ranch App',
     'tagline': 'An app I created for Cy-Ranch High School.',
+    'github': 'https://github.com/sshh12/CyRanch-App',
     'start': 'May 25, 2016'
   },
   {
     'title': 'CFISD App',
     'tagline': 'An app I created for Cypress Fairbanks ISD.',
+    'github': 'https://github.com/sshh12/CFISD-App',
     'start': 'May 14, 2018'
+  },
+  {
+    'title': 'CFISD API',
+    'tagline': 'The Unofficial CFISD API.',
+    'url': 'https://cfisdapi.herokuapp.com/',
+    'github': 'https://github.com/sshh12/CyRanch-App-Server',
+    'start': 'Jan 20, 2017'
+  },
+  {
+    'title': 'SSHH.IO',
+    'tagline': 'This website.',
+    'github': 'https://github.com/sshh12/SSHH.IO',
+    'start': 'Jul 5, 2018',
+    'wip': true
+  },
+  {
+    'title': '"Shrivu Bot"',
+    'tagline': 'A personalized discord bot.',
+    'github': 'https://github.com/sshh12/SSHHIO-Discord-Bot',
+    'start': 'Dec 30, 2017'
+  },
+  {
+    'title': 'StockMarketML',
+    'tagline': 'Predicting stocks with ML.',
+    'github': 'https://github.com/sshh12/StockMarketML',
+    'start': 'Sep 16, 2017'
+  },
+  {
+    'title': 'SchoolCode',
+    'tagline': 'A collection of scripts/algorithms that I wrote for/were inspired by school projects.',
+    'github': 'https://github.com/sshh12/SchoolCode',
+    'start': 'Jun 5, 2017'
+  },
+  {
+    'title': 'JavaIsWeird',
+    'tagline': 'A helpful collection of java stuff.',
+    'github': 'https://github.com/sshh12/JavaIsWeird',
+    'start': 'Sep 1, 2017'
+  },
+  {
+    'title': 'MLCode',
+    'tagline': 'A collection of scripts related to machine learning.',
+    'github': 'https://github.com/sshh12/MLCode',
+    'start': 'Aug 13, 2017'
+  },
+  {
+    'title': 'Build-A-Bill',
+    'tagline': 'Creating Legislation with AI',
+    'github': 'https://github.com/sshh12/Build-A-Bill',
+    'start': 'Jan 5, 2018'
+  },
+  {
+    'title': 'OverwatchML',
+    'tagline': 'Overwatch + AI',
+    'github': 'https://github.com/sshh12/OverwatchML',
+    'start': 'Aug 25, 2017'
+  },
+  {
+    'title': 'Webcam Anomaly Detection',
+    'tagline': 'An FTP server that logs anomalous motion from a camera stream.',
+    'github': 'https://github.com/sshh12/Webcam-Anomaly-Detection',
+    'start': 'Jun 4, 2017'
+  },
+  {
+    'title': 'Genetic Connect4',
+    'tagline': 'Using evolution to evolve a minimax connect4 AI.',
+    'github': 'https://github.com/sshh12/Genetic-Connect4',
+    'start': 'Jun 22, 2017'
+  },
+  {
+    'title': 'Students Visualization',
+    'tagline': 'A Visualization of the Cy-Ranch Student Body',
+    'github': 'https://github.com/sshh12/Students-Visualization',
+    'start': 'Apr 28, 2017'
+  },
+  {
+    'title': 'RanchCoin',
+    'tagline': 'A "Cryptocurrency" for EconFair',
+    'url': 'https://ranchcoin.firebaseapp.com/',
+    'github': 'https://github.com/sshh12/RanchCoin',
+    'start': 'Apr 10, 2018'
+  },
+  {
+    'title': 'FaceTweet',
+    'tagline': 'English Social Media Site Satire Project',
+    'url': 'https://facetweet-71a89.firebaseapp.com/',
+    'github': 'https://github.com/sshh12/FaceTweet',
+    'start': 'Mar 7, 2018'
+  },
+  {
+    'title': 'Rummy',
+    'tagline': 'A full-stack app for playing multiplayer Rummy.',
+    'github': 'https://github.com/sshh12/Rummy',
+    'start': 'Jan 11, 2018'
+  },
+  {
+    'title': 'Ballz Bot',
+    'tagline': 'A bot for Ballz.',
+    'github': 'https://github.com/sshh12/Ballz-Bot',
+    'start': 'Apr 9, 2017'
+  },
+  {
+    'title': 'PwnSSHH',
+    'tagline': 'Python wrapper for the MR3040.',
+    'github': 'https://github.com/sshh12/PwnSSHH',
+    'start': 'Jul 14, 2017'
   }
 ]
