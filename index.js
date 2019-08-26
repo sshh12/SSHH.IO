8bb0b47233e57af389a03fb6a343a11708992fb7
Shrivu Shankar <shrivu1122@gmail.com>
2019-08-26 12:02:00 -0500

    ✏️: Added Extra Endpoints

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ let app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => res.sendFile('public/index.html'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'projects.html')))
+app.get('/projects', (req, res) => res.sendFile(path.join(__dirname, 'public', 'projects.html')))
+app.get('/code', (req, res) => res.sendFile(path.join(__dirname, 'public', 'projects.html')))
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
