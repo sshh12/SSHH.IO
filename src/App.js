7737eece8aa60b89f41a18598f7b383f8c471515
Shrivu Shankar <shrivu1122@gmail.com>
2019-12-03 12:19:11 -0600

    🕹️: Search/Sort

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import Gallery from './components/Gallery';
 import Projects from './projects';
 import './App.css';
 
+function makeProjectRepr(project) {
+  let repr = Object.values(project).join('')
+  if(project.hack) { repr += ' greyhat whitehat ethical hacking security' }
+  if(project.github) { repr += ' github open-source' }
+  if(project.url) { repr += ' online webpage linked' }
+  if(project.team) { repr += ' team' + project.team }
+  if(project.wip) { repr += ' wip todo inprogress' }
+  if(project.private) { repr += ' private' }
+  else { repr += 'complete' }
+  if(/\bML\b/.test(project.tags)) { repr += ' machine learning' }
+  if(/\bRL\b/.test(project.tags)) { repr += ' reinforcement learning' }
+  if(/\bNLP\b/.test(project.tags)) { repr += ' natural language processing' }
+  if(/\bCV\b/.test(project.tags)) { repr += ' computer vision' }
+  repr = repr.toLocaleLowerCase();
+  return repr;
+}
+
 class App extends React.Component {
 
   componentDidMount() {
@@ -17,13 +34,31 @@ class App extends React.Component {
         title: project.title,
         desc: project.tagline,
         descs: project.desc || [],
-        img: project.thumb || '/images/no-pic.png'
+        img: project.thumb || '/images/no-pic.png',
+        date: new Date(project.start) || new Date(),
+        repr: makeProjectRepr(project),
+        meta: project
       };
     });
+    let sorts = [
+      {
+        name: 'Time',
+        sort: (a, b) => b.date.getTime() - a.date.getTime(),
+        title: (item) => `${item.title} (${item.meta.start})`
+      }, {
+        name: 'Coolness',
+        sort: (a, b) => b.meta.coolness - a.meta.coolness,
+        title: (item) => `${item.title} (${item.meta.coolness}/10)`
+      }, {
+        name: 'Language',
+        sort: (a, b) => b.meta.language.localeCompare(a.meta.language),
+        title: (item) => `${item.title} (${item.meta.language})`
+      }
+    ];
     return (
       <div className="App" id="wrapper">
         <Header />
-        <Gallery items={projects} />
+        <Gallery search={true} items={projects} sorts={sorts} />
         <Footer />
       </div>
     );
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Gallery({ items }) {
-  console.log(items.map(item => item.descs));
+function Gallery({ items, search, sorts }) {
+  let hasSorts = (sorts.length > 0);
+  let [sort, setSort] = useState(sorts[0]);
+  let [query, setQuery] = useState('');
+  items = items.concat();
+  if(hasSorts) {
+    items = items.sort(sort.sort);
+  }
+  if(query.length > 0) {
+    items = items.filter(item => item.repr.includes(query.toLocaleLowerCase()));
+  }
   return (
-    <div id="main">
-      {items.map(item => 
-        <article key={item.title} className="thumb">
-          <a href={item.img} className="image"><img src={item.img} alt="" /></a>
-          <h2 style={{fontWeight: '700'}}>{item.title}</h2>
-          <p>{item.desc}</p>
-          {item.descs.length > 0 && <hr/>}
-          {item.descs.length > 0 && <ul>{item.descs.map(text => <li key={text}>{text}</li>)}</ul>}
-        </article>)}
+    <div>
+      <div>
+        {search && 
+          <input onChange={(e) => setQuery(e.target.value)}
+            style={{fontWeight: '600'}} type="text" placeholder="Search..." />}
+        {hasSorts && <ul class="actions small" style={{marginTop: '27px'}}>
+          {sorts.map((st, i) => <li key={st.name} style={{margin: 'auto'}}>
+            <a onClick={() => setSort(sorts[i])}
+              class={`button small${st.name == sort.name ? ' primary': ''}`}>{st.name}</a></li>)}
+        </ul>}
+      </div>
+      <div id="main">
+        {items.map(item =>
+          <article key={item.title} className="thumb">
+            <a href={item.img} className="image"><img src={item.img} alt="" /></a>
+            <h2 style={{ fontWeight: '700' }}>{hasSorts ? sort.title(item) : item.title}</h2>
+            <p>{item.desc}</p>
+            {item.descs.length > 0 && <hr />}
+            {item.descs.length > 0 && <ul>{item.descs.map(text => <li key={text}>{text}</li>)}</ul>}
+          </article>)}
+      </div>
     </div>
   );
 }
