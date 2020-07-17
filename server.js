f9c130f93e95b907f345474f6ec41541d77f9e04
Shrivu Shankar <shrivu1122@gmail.com>
2020-07-17 17:07:30 -0500

    tweaks

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require('express');
 const path = require('path');
-const auth = require('basic-auth');
-const PORT = process.env.PORT || 5000;
 
 let app = express();
 app.use(express.urlencoded());
-
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/resume', (req, res) => res.redirect('https://forms.gle/m7FMe8beVMH5Z33d9'));
 
 let reactBuild = path.join(__dirname, 'build', 'index.html');
 
 app.get('/', (req, res) => res.sendFile(reactBuild));
 app.get('/code', (req, res) => res.sendFile(reactBuild));
 app.get('/code/:ident', (req, res) => res.sendFile(reactBuild));
-app.get('/photos', (req, res) => res.sendFile(reactBuild));
+app.get('/resume', (req, res) => res.redirect('https://forms.gle/m7FMe8beVMH5Z33d9'));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Listening on ${PORT}`);
 });
