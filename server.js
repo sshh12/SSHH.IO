9012426a44cf149a0cbe9feb24671f541c5a3b9d
Shrivu Shankar <shrivu1122@gmail.com>
2020-12-16 20:16:58 -0600

    update server

--- server.js
+++ server.js
@@ -8,8 +8,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 let reactBuild = path.join(__dirname, 'build', 'index.html');
 
 app.get('/', (req, res) => res.sendFile(reactBuild));
-app.get('/code', (req, res) => res.sendFile(reactBuild));
-app.get('/code/:ident', (req, res) => res.sendFile(reactBuild));
 app.get('/resume', (req, res) => res.redirect('https://forms.gle/m7FMe8beVMH5Z33d9'));
 
 app.listen(process.env.PORT || 5000, () => {
